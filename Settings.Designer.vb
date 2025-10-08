1b063e80b8f9b6764a9e106a9ac2a098901a2ac7
lethehung694-commits <lethehung694@gmail.com>
2025-10-08 23:01:33 +0700

    Add files via upload

--- Settings.Designer.vb
+++ Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
 
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
-        Friend ReadOnly Property Settings() As Global.lehungth29._14.My.MySettings
+        Friend ReadOnly Property Settings() As Global.BÀI_CỦA_PHÚ_MẬP.My.MySettings
             Get
-                Return Global.lehungth29._14.My.MySettings.Default
+                Return Global.BÀI_CỦA_PHÚ_MẬP.My.MySettings.Default
             End Get
         End Property
     End Module
